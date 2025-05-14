6770fb5302457abd600da700a4ec52682abe5b7f
ZanxuWang <144561853+ZanxuWang@users.noreply.github.com>
2025-05-14 14:57:01 -0400

    Add files via upload

--- config/EEG-Diff/_base_/noise_scheduler.py
+++ config/EEG-Diff/_base_/noise_scheduler.py
@@ -1,11 +1,11 @@
 from EEG import DDIMScheduler
 noise_scheduler=dict(
         type=DDIMScheduler,
-        beta_end=0.02,
+        beta_end=0.01,
         beta_schedule="linear",
         beta_start=0.0001,
         clip_sample=True,
-        num_train_timesteps=200,
+        num_train_timesteps=1500,
         prediction_type="epsilon",
         set_alpha_to_one=True,
         steps_offset=0,
